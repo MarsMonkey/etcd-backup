2571c7af7be3d01f7f3df77dbfa6b8cd9a505eef
mars <marsmonkey94@163.com>
2021-11-10 18:41:43 +0800

    u

--- controllers/etcdbackup_controller.go
+++ controllers/etcdbackup_controller.go
@@ -18,11 +18,17 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	etcdv1alpha1 "github.com/MarsMonkey/etcd-backup/api/v1alpha1"
 )
@@ -59,4 +65,115 @@ func (r *EtcdBackupReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&etcdv1alpha1.EtcdBackup{}).
 		Complete(r)
+}
+
+// backupState 包含 EtcdBackup 真实和期望的状态 (这里的状态并不是说status)
+type backupState struct {
+	backup *etcdv1alpha1.EtcdBackup // EtcdBackup 对象本身
+	actual *backupStateContainer // 真实状态
+	desired *backupStateContainer //期望状态
+}
+
+// backupStateContainer contains the state of EtcdBackup
+type backupStateContainer struct {
+	pod *corev1.Pod
+}
+
+// setStateActual 用于设置 backupState 的真实状态
+func (r *EtcdBackupReconciler ) setStateActual (ctx context.Context, state *backupState) error {
+	var actual backupStateContainer
+
+	key := client.ObjectKey {
+		Name: 	state.backup.Name,
+		Namespace: state.backup.Namespace,
+	}
+
+	// get pod
+	actual.pod = &corev1.Pod{}
+	if err := r.Get(ctx, key, actual.pod); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			return fmt.Errorf("getting pod err: %s", err)
+		}
+		actual.pod = nil
+	}
+
+	// 填充当前真实的状态
+	state.actual = &actual
+	return nil
+}
+
+// setStateDesired 用于设置 backupState 的期望状态 (根据 EtcdBackup 对象)
+func (r *EtcdBackupReconciler) setStateDesired(state *backupState) error {
+	var desired backupStateContainer
+
+	// 创建一个管理的pod用于执行备份操作
+	pod, err := podForBackup(state.backup, r.BackupAgentImage)
+	if err != nil {
+		return fmt.Errorf("computing pod for backup error: %q", err)
+	}
+	// controller reference
+	if err := controllerutil.SetControllerReference(state.backup, pod, r.Scheme); err != nil {
+		return fmt.Errorf("setting pod controller reference error: %s", err)
+	}
+	desired.pod = pod
+	// 获得期望的对象
+	state.desired = &desired
+	return nil
+}
+
+// getState 用于获取当前应用的整个状态，然后才方便判断下一步动作
+func (r EtcdBackupReconciler) getState(ctx context.Context, req ctrl.Request) (*backupState, error) {
+	var state backupState
+
+	// 获取 EtcdBackup 对象
+	state.backup = &etcdv1alpha1.EtcdBackup{}
+	if err := r.Get(ctx, req.NamespacedName, state.backup); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			return nil, fmt.Errorf("getting backup error: %s", err)
+		}
+		// 被删除了则直接忽略
+		state.backup = nil
+		return &state, nil
+	}
+
+	// 获取当前备份的真实状态
+	if err := r.setStateActual(ctx, &state); err != nil {
+		return nil, fmt.Errorf("setting actual state error: %s", err)
+	}
+
+	// 获取当前期望的状态
+	if err := r.setStateDesired(&state); err != nil {
+		return nil, fmt.Errorf("setting desired state error: %s", err)
+	}
+
+	return &state, nil
+}
+
+// podForBackup 创建一个pod运行备份任务
+func podForBackup(backup *etcdv1alpha1.EtcdBackup, image string) (*corev1.Pod, error) {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: backup.Name,
+			Namespace: backup.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name: "backup-agent",
+					Image: image,
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceCPU: resource.MustParse("100m"),
+							corev1.ResourceMemory: resource.MustParse("50Mi"),
+						},
+						Limits: corev1.ResourceList{
+							corev1.ResourceCPU: resource.MustParse("100m"),
+							corev1.ResourceMemory: resource.MustParse("50Mi"),
+						},
+					},
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyNever,
+		},
+	}, nil
 }
